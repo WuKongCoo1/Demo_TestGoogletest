c101f1e2255957b6171bbf632a6ee3c3ddd567b5
Jacky Walker <wu_kong_coo1@163.com>
2019-12-08 18:14:43 +0800

    Init

--- TestGoogletest/main.cpp
+++ TestGoogletest/main.cpp
@@ -7,9 +7,15 @@
 //
 
 #include <iostream>
+#include "gtest/gtest.h"
+#include <numeric>
+#include <vector>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    ::testing::InitGoogleTest(&argc, (char **)argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(TestSuiteName, TestName) {
+    EXPECT_EQ(1, 1);
 }
