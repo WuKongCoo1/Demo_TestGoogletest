88505132b8c265503f41f6e2a3b155a07d2c3b73
Jacky Walker <wu_kong_coo1@163.com>
2019-12-08 16:20:24 +0800

    Initial Commit

---
+++ TestGoogletest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestGoogletest
+//
+//  Created by LINE Works on 2019/12/8.
+//  Copyright © 2019 Jacky Walker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
